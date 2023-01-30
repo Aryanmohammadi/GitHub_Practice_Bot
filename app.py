198651b01e92f4ed03cb0b019e67585070d372e8
Aryanmohammadi <aryanmohammdi@yahoo.com>
2023-01-30 06:56:49 +0330

    start p-t-b

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
-# Import the telegram bot framwork and everythings we need
+import logging
 from telegram import Update
-from telegram.ext import Updater,CallbackContext,CommandHandler
-#import api key
-from decouple import config
-# create  api_secret var and assign it to our api code
-# you should make all of the global variables here
-api_token = config("API_TOKEN")
-updater = Updater (api_token)
-dispatcher = updater.dispatcher
-#Create function that accept a string and returns same string(echo bot)
-def start(update:Update,context:CallbackContext):
-    chat_id = update.message.chat_id
-    message_id = update.message.message_id
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
-    context.bot.send_message(chat_id=chat_id, text="Hello")
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-dispatcher.add_handler(CommandHandler(['start','s','START'],start))
-updater.start_polling()
-#don't forget to make if __main__ here
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Echo the user message."""
+
+    await update.message.reply_text(update.message.text)
+
+async def echo_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Echo the user photo."""
+    
+    await update.message.reply_photo(update.message.photo)
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('5800137374:AAFszl4NSS7k_O7FEwk2fBB-c67uRbqXMzw').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.PHOTO, echo_photo))
+    application.run_polling()
