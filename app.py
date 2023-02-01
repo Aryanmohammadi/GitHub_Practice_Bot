a74fe1d31dd0360af0d46bcc63235d80da084aa8
Aryanmohammadi <aryanmohammdi@yahoo.com>
2023-02-01 14:35:03 +0330

    echo image and add openCV to project

--- app.py
+++ app.py
@@ -1,34 +1,78 @@
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+import os
+import pathlib
+from telegram import Update, ForceReply
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import cv2 as cv
 
+
+# Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+  format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+  level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+  """Send a message when the command /start is issued."""
+  user = update.effective_user
+  await update.message.reply_html(
+    rf"Hi {user.mention_html()}!",
+    reply_markup=ForceReply(selective=True),
+  )
+
+
+async def help_command(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> None:
+  """Send a message when the command /help is issued."""
+  await update.message.reply_text("Hamma Gyan!")
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
-    """Echo the user message."""
-
-    await update.message.reply_text(update.message.text)
-
-async def echo_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-
-    """Echo the user photo."""
-    
-    await update.message.reply_photo(update.message.photo)
+  """Echo the user message."""
+  await update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('5800137374:AAFszl4NSS7k_O7FEwk2fBB-c67uRbqXMzw').build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-    application.add_handler(MessageHandler(filters.PHOTO, echo_photo))
-    application.run_polling()
+async def echo_image(update, context):
+  user = update.message.from_user
+  image_file = await update.message.photo[-1].get_file()
+  await image_file.download_to_drive("userphoto.jpg")
+  logger.info("Photo of %s: %s", user.first_name, "userphoto.jpg")
+  path = 'userphoto.jpg'
+  image = cv.imread(path)
+  window_name = 'Image'
+  font = cv.FONT_HERSHEY_SIMPLEX
+  org = (50, 50)
+  fontScale = 1
+  color = (255, 0, 0)
+  thickness = 2
+  image = cv.putText(image, 'hamma gyan', org, font,fontScale, color, thickness, cv.LINE_AA)
+  cv.imwrite('userphoto.jpg', image)
+  # cv.imshow(window_name, image)
+  # cv.waitKey(0)
+  await context.bot.send_photo(chat_id=update.message.chat_id,
+                               photo=open('userphoto.jpg' , "rb"))
+
+
+def main() -> None:
+  """Start the bot."""
+  # Create the Application and pass it your bot's token.
+  application = Application.builder().token(Token).build()
+
+  # on different commands - answer in Telegram
+  application.add_handler(CommandHandler("start", start))
+  application.add_handler(CommandHandler("help", help_command))
+
+  # on non command i.e message - echo the message on Telegram
+  application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND,
+                                         echo))
+  application.add_handler(MessageHandler(filters.PHOTO, echo_image))
+
+  # Run the bot until the user presses Ctrl-C
+  application.run_polling()
+
+
+if __name__ == "__main__":
+  main()
